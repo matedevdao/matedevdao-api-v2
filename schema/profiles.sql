32e617c2e60f3140c37ce09cae0a272da2d05704
yj.gaia <gaiaprotocolnft@gmail.com>
2025-12-16 23:16:50 +0900

    Add new fields to profiles table for NFT integration
    - Expanded the `profiles` table schema to include `primary_nft_contract_address` and `primary_nft_token_id` for enhanced NFT tracking.
    - Added `created_at` and `updated_at` fields to manage timestamps for profile records, ensuring better data integrity and tracking.

--- schema/profiles.sql
+++ schema/profiles.sql
@@ -1,5 +1,11 @@
 CREATE TABLE profiles (
   account TEXT PRIMARY KEY,
   nickname TEXT,
-  bio TEXT
+  bio TEXT,
+
+  primary_nft_contract_address TEXT,
+  primary_nft_token_id TEXT,
+
+  created_at INTEGER NOT NULL DEFAULT (strftime('%s','now')),
+  updated_at INTEGER
 );
